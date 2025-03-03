fcb5c7e86912c8d03068b93f882868ebca772807
onh711 <fmv811@yahoo.co.jp>
2025-03-04 00:30:35 +0900

    update

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -86,7 +86,7 @@ const TopicsWrapper = styled.div`
 `;
 
 const TopicsContent = styled.div`
-  margin: 30px 50px 0;
+  margin: 30px 45px 0;
   max-width: 1300px;
   border-top: 1px solid;
   animation: slideIn 1s cubic-bezier(0.25, 1, 0.5, 1) forwards;
