1c2754d67c87f45124e3d19acabb6d9541403cb9
onh711 <fmv811@yahoo.co.jp>
2025-03-18 22:29:46 +0900

    test update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { Top } from "./pages/Top";
 import { MenuList } from "./pages/MenuList";
 import { TopicsLists } from "./pages/TopicsLists";
@@ -10,24 +10,21 @@ import { ConfirmPage } from "./pages/ConfirmPage.jsx";
 import { SubmitCompletePage } from "./pages/SubmitCompletePage.jsx";
 import { ContactFormPage } from "./pages/ContactFormPage.jsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        { path: "/", element: <Top /> },
-        { path: "/topic", element: <TopicsLists /> },
-        { path: "/topic/:id", element: <TopicsPage /> },
-        { path: "/menu", element: <MenuList /> },
-        { path: "/form", element: <ContactFormPage /> },
-        { path: "/confirm", element: <ConfirmPage /> },
-        { path: "/submit", element: <SubmitCompletePage /> },
-      ],
-    },
-  ],
-  { basename: "/portfolio/" }
-);
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Top /> },
+      { path: "/topic", element: <TopicsLists /> },
+      { path: "/topic/:id", element: <TopicsPage /> },
+      { path: "/menu", element: <MenuList /> },
+      { path: "/form", element: <ContactFormPage /> },
+      { path: "/confirm", element: <ConfirmPage /> },
+      { path: "/submit", element: <SubmitCompletePage /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
